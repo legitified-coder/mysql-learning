0929b2ceac322b414d12927acaeaf074e0ecc204
DagmNiko <dagmniko79@gmail.com>
2023-12-04 18:43:34 +0300

    mosh course update

--- main.sql
+++ main.sql
@@ -1,2 +1,12 @@
-create database MyDB;
+# DON'T FORGET TO COMMENT OR DELETE EACH STEP AFTER YOU EXECUTE THEM.
+
+# -- STEP 1 | 
+SELECT * from sql_store.customers WHERE state IN ('VA', 'LA', 'CO', 'FL');
+
+
+
+
+# I hope you found this helpful❤
+
+# Stay Awake
 #legitified✔
